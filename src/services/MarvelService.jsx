a3a13ece77b22768cdc07523b886f768c62057f9
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2024-06-25 23:58:01 +0500

    first commit

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -3,11 +3,11 @@ class MarvelService{
     _apiKey = 'apikey=db59875c1437145e2d1a9e1621bc7b87';
     getResource = async (url) => {
         let res = await fetch(url);
-
+    
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
 
@@ -23,11 +23,13 @@ class MarvelService{
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description: char.description,
-            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension, 
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
